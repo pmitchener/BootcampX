6ae32f357f7c143c8cbbf0b5e913a789b51a2e28
Patrick Mitchener <pmitchener@rogers.com>
2020-08-20 01:52:06 +0000

    Add code to take care of sql injection issues.

--- bootcamp_app/student.js
+++ bootcamp_app/student.js
@@ -8,14 +8,14 @@ const pool = new Pool({
 });
 
 const [, , cohortName, recordLimit] = process.argv;
+const values = [`%${cohortName}%`, recordLimit];//store all potentially malicius strings into an array.
 
-pool.query(`
-SELECT students.id, students.name, cohorts.name AS cohort
+const sql = `SELECT students.id, students.name, cohorts.name AS cohort
 FROM students
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '${cohortName}%'
-LIMIT ${recordLimit};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;`;
+pool.query(sql, values)
 .then(res => {
   res.rows.forEach(dbResultSet => {
     console.log(`${dbResultSet.name} has an id of ${dbResultSet.id} and was in the ${dbResultSet.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,17 +8,19 @@ const pool = new Pool({
 });
 
 const [, , cohortName] = process.argv;
+const values = [`${cohortName}`];//store all potentially malicius strings into an array.
 
-pool.query(`
+const sql = `
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
 JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name = '${cohortName}'
+WHERE cohorts.name = $1
 GROUP by teachers.name, cohorts.name
 ORDER BY teachers.name;
-`)
+`;
+pool.query(sql, values)
 .then(res => {
   res.rows.forEach(dbResultSet => {
     console.log(`${dbResultSet.cohort}: ${dbResultSet.teacher}`);
